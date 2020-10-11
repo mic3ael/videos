61cbed36f15a1b016d76ea9ffd7c3ffd91f1ccb1
Michael Horojanski <mic3ael@gmail.com>
2020-10-11 23:07:15 +0300

    add gorm

--- entities/video.go
+++ entities/video.go
@@ -1,15 +1,22 @@
 package entities
 
+import "time"
+
 type Person struct {
-	FirstName string `json:"firstName" binding:"required"`
-	LastName  string `json:"lastName" binding:"required"`
+	ID        uint64 `gorm:"primary_key;auto_increment" json:"id"`
+	FirstName string `json:"firstName" binding:"required" gorm:"type:varchar(32)"`
+	LastName  string `json:"lastName" binding:"required" gorm:"type:varchar(32)"`
 	Age       int8   `json:"age" binding:"gte=1,lte=130"`
-	Email     string `json:"email" validate:"required,email"`
+	Email     string `json:"email" validate:"required,email" gorm:"type:varchar(256)"`
 }
 
 type Video struct {
-	Title       string `json:"title" binding:"min=2,max=20" validate:"is-cool"`
-	Description string `json:"description binding:"min=2,max=20"`
-	URL         string `json:"url" binding:"required,url"`
-	Author      Person `json:"author" binding:"required"`
+	ID          uint64    `gorm:"primary_key;auto_increment" json:"id"`
+	Title       string    `json:"title" binding:"min=2,max=20" gorm:"type:varchar(100)"`
+	Description string    `json:"description binding:"min=2,max=20" gorm:"type:varchar(200)"`
+	URL         string    `json:"url" binding:"required,url" gorm:"type:varchar(256);UNIQUE"`
+	Author      Person    `json:"author" binding:"required" gorm:"foreignkey:PersonID"`
+	PersonID    uint64    `json:"-"`
+	CreatedAt   time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt   time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
